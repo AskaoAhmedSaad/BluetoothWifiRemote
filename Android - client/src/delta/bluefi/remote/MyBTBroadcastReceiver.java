ff3d8dfb92836c44f711aec094c67094fcf01ee3
askao <askao@askao-.com>
2014-09-29 08:27:40 +0200

    GNU license

--- Android - client/src/delta/bluefi/remote/MyBTBroadcastReceiver.java
+++ Android - client/src/delta/bluefi/remote/MyBTBroadcastReceiver.java
@@ -1,3 +1,19 @@
+/* < BluetoothWifiRemote app (android touchpad for desktop - send files from android device to Desktop 
+	- scroll slides in datashow presentation) - accross wifi and blutooth connection (tools: android / j2se).>
+    Copyright (C) 2014 Askao(AhmedSaad)-Omar EzzElDien
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */  	
 package delta.bluefi.remote;
 
 import android.bluetooth.BluetoothAdapter;
